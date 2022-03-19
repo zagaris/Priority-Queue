9c543c6f64fbfe9d33a67e7de950640258b08d46
Aristomenis Zagakos <csd3696@csd.uoc.gr>
2022-03-20 01:34:29 +0200

    add test

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,8 +4,8 @@ describe('Check if the items are sorted', () => {
   it('works', () => {
     const q = new PriorityQueue({
       comparatorFn: (a,b) => a - b,
-      intialValues: [32, 5, 44, 10, 1]
+      intialValues: [35, 4, 48, 12, 1]
     });
-    expect(q.heapsort()).toEqual([1, 5, 10, 32, 44]);
+    expect(q.heapsort()).toEqual([1, 4, 12, 35, 48]);
   });
 });
